cb6ab0c553ea42359b219b77429075eadcc46bbf
michipase <michipase@gmail.com>
2021-01-22 21:26:27 +0100

    add base page routing

---
+++ views/about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookique | about</title>
+</head>
+<body>
+    noi siamo un team fortissimo!
+</body>
+</html>

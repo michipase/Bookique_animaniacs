4879b4c0a50377a7759d0f4749fab7e24b196390
michipase <michipase@gmail.com>
2021-01-25 18:58:05 +0100

    test router, htaccess

--- index.php
+++ index.php
@@ -1,27 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
 
-$request = $_SERVER['REQUEST_URI'];
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-switch ($request) {
-    case '/' :
-        require __DIR__ . '/views/index.php';
-        break;
-    case '' :
-        require __DIR__ . '/views/index.php';
-        break;
-    case '/about' :
-        require __DIR__ . '/views/about.php';
-        break;
-    case '/login' :
-        require __DIR__ . '/views/about.php';
-        break;
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/Bookique_animaniacs/">Home</a>
+            <a class="nav-item nav-link" href="/Bookique_animaniacs/about">About</a>
+            <a class="nav-item nav-link" href="/Bookique_animaniacs/login">login</a>
+            <a class="nav-item nav-link" href="/Bookique_animaniacs/registration">registration</a>
+        </div>
+    </nav>
 
-    // add here new routes
+    <?php
+
+    require 'db.php';
+
+    $request = $_SERVER['REQUEST_URI'];
+
+
+    try {
+        $request = str_replace('/Bookique_animaniacs', '', $request);
+    } catch (\Throwable $th) {
+
+    }
+
 
     
-    default:
-        http_response_code(404);
-        require __DIR__ . '/views/404.php';
-        break;
-}
+    switch ($request) {
+        case '/' :
+            require __DIR__ . '/views/index.php';
+            break;
+        case '/about' :
+            require __DIR__ . '/views/about.php';
+            break;
+        case '/registration':
+            require __DIR__ . '/views/registration.php';
+            break;
+        case '/login' :
+            require __DIR__ . '/views/login.php';
+            break;
+
+        default:
+            http_response_code(404);
+            require __DIR__ . '/404.php';
+            break;
+    }
 ?>
+</body>
+</html>
--- views/index.php
+++ views/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookique</title>
+    <title>Home</title>
 </head>
 <body>
-    index bello bello
+    HOME
 </body>
 </html>

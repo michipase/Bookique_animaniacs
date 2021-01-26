50a01ebed4029ca883cf2a3de315874a0bf115e9
michipase <michipase@gmail.com>
2021-01-26 19:14:51 +0100

    add registration

--- views/registration.php
+++ views/registration.php
@@ -1,115 +1,97 @@
+<?php
+
+    function register() {
+
+        $conn = openConn();
+
+        if (isset($_POST['register_user'])) {
+
+            $username = mysqli_real_escape_string($conn, $_POST['username']);
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $password_1 = mysqli_real_escape_string($conn, $_POST['password_1']);
+            $password_2 = mysqli_real_escape_string($conn, $_POST['password_2']);
+        
+            // form validation: ensure that the form is correctly filled ...
+            // by adding (array_push()) corresponding error unto $errors array
+            if (empty($username)) { array_push($errors, "Username is required"); }
+            if (empty($email)) { array_push($errors, "Email is required"); }
+            if (empty($password_1)) { array_push($errors, "Password is required"); }
+            if ($password_1 != $password_2) {
+                array_push($errors, "The two passwords do not match");
+            }
+        
+            // first check the database to make sure 
+            // a user does not already exist with the same username and/or email
+            $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+            $result = mysqli_query($conn, $user_check_query);
+            $user = mysqli_fetch_assoc($result);
+            
+            if ($user) { // if user exists
+            if ($user['username'] === $username) {
+                array_push($errors, "Username already exists");
+            }
+        
+            if ($user['email'] === $email) {
+                array_push($errors, "email already exists");
+            }
+            }
+        
+            // Finally, register user if there are no errors in the form
+            if (count($errors) == 0) {
+                $password = md5($password_1);//encrypt the password before saving in the database
+        
+                $query = "INSERT INTO users (username, email, password) 
+                        VALUES('$username', '$email', '$password')";
+                mysqli_query($conn, $query);
+                $_SESSION['username'] = $username;
+                $_SESSION['success'] = "You are now logged in";
+                header('location: index.php');
+            }
+        }
+
+        closeConn();
+    }
+?>
+
+
 <div class="container register">
     <div class="row">
+
         <div class="col-md-3 register-left">
-            <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
-            <h3>Welcome</h3>
-            <p>You are 30 seconds away from earning your own money!</p>
-            <input type="submit" name="" value="Login"/><br/>
+            <img src="" alt="">
         </div>
+
         <div class="col-md-9 register-right">
-            <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Employee</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Hirer</a>
-                </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    <h3 class="register-heading">Apply as a Employee</h3>
-                    <div class="row register-form">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="First Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Last Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control"  placeholder="Confirm Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <div class="maxl">
-                                    <label class="radio inline"> 
-                                        <input type="radio" name="gender" value="male" checked>
-                                        <span> Male </span> 
-                                    </label>
-                                    <label class="radio inline"> 
-                                        <input type="radio" name="gender" value="female">
-                                        <span>Female </span> 
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Your Email *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <select class="form-control">
-                                    <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
-                                    <option>What is your Birthdate?</option>
-                                    <option>What is Your old Phone Number</option>
-                                    <option>What is your Pet Name?</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
-                            </div>
-                            <input type="submit" class="btnRegister"  value="Register"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                    <h3  class="register-heading">Apply as a Hirer</h3>
-                    <div class="row register-form">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="First Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Last Name *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control" placeholder="Email *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" maxlength="10" minlength="10" class="form-control" placeholder="Phone *" value="" />
-                            </div>
 
+            <form method="POST" action="">
 
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Confirm Password *" value="" />
-                            </div>
-                            <div class="form-group">
-                                <select class="form-control">
-                                    <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
-                                    <option>What is your Birthdate?</option>
-                                    <option>What is Your old Phone Number</option>
-                                    <option>What is your Pet Name?</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="`Answer *" value="" />
-                            </div>
-                            <input type="submit" class="btnRegister"  value="Register"/>
-                        </div>
-                    </div>
+                <div class="input-group">
+                    <label>Username</label>
+                    <input type="text" name="username" value="">
                 </div>
-            </div>
+            
+                <div class="input-group">
+                    <label>Email</label>
+                    <input type="email" name="email" value="">
+                </div>
+            
+                <div class="input-group">
+                    <label>Password</label>
+                    <input type="password" name="password_1">
+                </div>
+
+                <div class="input-group">
+                    <label>Confirm password</label>
+                    <input type="password" name="password_2">
+                </div>
+
+                <div class="input-group">
+                    <button type="submit" class="btn" name="register_user">Register</button>
+                </div>
+                <p>
+                    Already a member? <a href="login.php">Sign in</a>
+                </p>
+            </form>  
         </div>
     </div>
-
 </div>

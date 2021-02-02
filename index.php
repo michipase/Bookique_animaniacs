cd3e290c7fc649ed6118d8f55558f093340d8f99
stefan891 <razvan891@gmail.com>
2021-02-02 19:12:37 +0100

    aggiustamento parte di password che generava problemi nella registrare i dati sul database, aggiunta riga 4 in index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 $request = $_SERVER['REQUEST_URI'];
+$request = str_replace('Bookique_animaniacs/', '', $request);
 
 switch ($request) {
     case '/' :
@@ -12,15 +13,30 @@ switch ($request) {
     case '/login' :
         require __DIR__ . '/views/login.php';
         break;
-    case '/signup':
-        require __DIR__ . '/views/signup.php';
+    case '/singnup':
+        require __DIR__ . '/views/singnup.php';
         break;
 
     // add here new routes
     
     default:
         http_response_code(404);
-        require __DIR__ . '/views/404.php';
+        require __DIR__ . '/404.php';
         break;
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="views/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+</head>
+<body>
+
+</body>
+</html>
+
